d15422639dbf36ca34cbf58c3e4cc6762ffcc3d3
tohoilam <61353084+tohoilam@users.noreply.github.com>
2020-08-07 23:19:57 +0800

    created react app and fetched access token

---
+++ src/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './login.css';
+import loginUrl from './loginUrl';
+
+function Login() {
+  return (
+    <div className="login">
+      <a href={loginUrl}>Login with your Spotify </a>
+      {/* Spotify Logo */}
+      {/* Login with spotify component */}
+    </div>
+  )
+}
+
+export default Login;
